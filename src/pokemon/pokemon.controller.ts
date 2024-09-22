8fae27e48af45fd274cfcf48b72f519c81817fa8
valenzuela21 <vlzdavid12@outlook.com>
2024-09-22 16:09:48 -0500

    API Pokemon

--- src/pokemon/pokemon.controller.ts
+++ src/pokemon/pokemon.controller.ts
@@ -1,13 +1,29 @@
 import { Controller, Get, Param, Query } from '@nestjs/common';
 import { PokemonService } from './pokemon.service';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiQuery, ApiResponse, ApiTags } from "@nestjs/swagger";
 
 @ApiTags('pokemon')
 @Controller('pokemon')
+@ApiResponse({ status: 200, description: 'Successfully retrieved list of Pokemons.' })
+@ApiResponse({ status: 400, description: 'Invalid input.' })
 export class PokemonController {
   constructor(private readonly pokemonService: PokemonService) {}
 
   @Get()
+  @ApiQuery({
+    name: 'limit',
+    required: false,
+    type: String,
+    description: 'Number of items to return',
+    example: '100',
+  })
+  @ApiQuery({
+    name: 'offset',
+    required: false,
+    type: String,
+    description: 'Offset for pagination',
+    example: '1',
+  })
   async findAll(
     @Query('limit') limit: string = '100',
     @Query('offset') offset: string = '1',
